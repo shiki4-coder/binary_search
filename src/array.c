72578d379ad291d08120a792bb2ab8190d3f006f
Kohei Wakita <shikifourseasons@icloud.com>
2020-07-03 19:20:12 +0900

    revised

--- src/array.c
+++ src/array.c
@@ -6,13 +6,21 @@ int A[100000];
 
 
 int main(){
-  int i, lb, ub;
-  scanf("%d%d", &n, &k);
-  for(i = 0; i < n; i++){
-    scanf("%d", &A[i]);
-  }
+    int i, lb, ub;
+    scanf("%d%d", &n, &k);
+    for(i = 0; i < n; i++){
+        scanf("%d", &A[i]);
+    }
 
-
+    lb = -1;
+    ub = n;
+    int mid;
+    while(ub-lb >1){
+        mid = (ub+lb)/2;
+        if (A[mid]>=k) ub = mid;
+        else lb = mid;
+    }
+    printf("%d\n",ub);
 
   return 0;
 }
