e0e6f85f1125e1a82439d1f74a94abf2e67a4833
Kohei Wakita <shikifourseasons@icloud.com>
2020-07-06 19:16:37 +0900

    revised

--- src/apple.c
+++ src/apple.c
@@ -4,14 +4,40 @@ int n;
 int k;
 int A[100000];
 
+int p(int x,int A[], int n,int k){
+    int sum =0;
+    for(int i = 0; i <n;i++){
+        sum += (A[i]+x-1)/x ;
+    }
+    if (sum <= k){
+        return 1;
+    }
+    else {
+        return 0;
+    }
+}
 
 int main(){
-  int i, lb, ub;
-  scanf("%d%d", &n, &k);
-  for(i = 0; i < n; i++){
-    scanf("%d", &A[i]);
-  }
+    int i, lb, ub;
+    scanf("%d%d", &n, &k);
+    for(i = 0; i < n; i++){
+        scanf("%d", &A[i]);
+    }
 
+    lb =0;
+    ub =A[n-1]+1;
+    while(ub-lb >1){
+        int mid = (lb+ub)/2;
+        if (p(mid,A,n,k)){
+            ub = mid;
+        }
+        else{
+            lb = mid;
+        }
+    }
+    printf("%d\n",ub);
+
+//printf("%d\n",p(A[3],A,n,k));
 
   return 0;
 }
