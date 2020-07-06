8f14323420fd5407a8a7770bc39724dc6b0c449d
Kohei Wakita <shikifourseasons@icloud.com>
2020-07-06 21:57:59 +0900

    revised

--- src/spear.c
+++ src/spear.c
@@ -3,15 +3,36 @@
 int n;
 int k;
 int A[100000];
-
+int p(int x, int A[],int n,int k){
+    int sum =0;
+    for (int i =0;i<n;i++){
+        sum+= A[i]/x;
+    }
+    if (sum>=k){
+        return 0;
+    }
+    else{
+        return 1;
+    }
+}
 
 int main(){
-  int i, lb, ub;
-  scanf("%d%d", &n, &k);
-  for(i = 0; i < n; i++){
-    scanf("%d", &A[i]);
-  }
+    int i, lb, ub;
+    scanf("%d%d", &n, &k);
+    for(i = 0; i < n; i++){
+        scanf("%d", &A[i]);
+    }
+    ub = A[n-1]+1;
+    lb = 0;
+    while(ub-lb>1){
+        int mid =(lb+ub)/2;
+        if (p(mid,A,n,k)){
+            ub = mid;
+        }
+        else lb =mid;
+    }
+    printf("%d\n",lb);
 
 
-  return 0;
+    return 0;
 }
